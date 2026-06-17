f3859935bc47754467e1891b9b12168a0cc830ab
WarlessonSabino <contatowarlessonsabino@gmail.com>
2026-06-17 14:54:05 -0300

    Update dbconfig.js

--- dbconfig.js
+++ dbconfig.js
@@ -2,8 +2,8 @@ export default {
     host: 'pharmesjf2.ddns.net',
     port: 3050,
     database: 'C:/FCerta/DB/ALTERDB.ib', 
-    user: 'INT_API',
-    password: 'TI@406rx10',
+    user: 'SYSDBA',
+    password: 'masterkey',
     lowercase_keys: false, 
     role: null, 
     pageSize: 4096
