a9c0fd867a53955fd931d8c7ad5ded7187fcc773
WarlessonSabino <contatowarlessonsabino@gmail.com>
2026-06-17 13:53:08 -0300

    Troca de Acessos

--- dbconfig.js
+++ dbconfig.js
@@ -2,8 +2,8 @@ export default {
     host: 'pharmesjf2.ddns.net',
     port: 3050,
     database: 'C:/FCerta/DB/ALTERDB.ib', 
-    user: 'SYSDBA',
-    password: 'masterkey',
+    user: 'INT_API',
+    password: ' 560vzUN:*M£J',
     lowercase_keys: false, 
     role: null, 
     pageSize: 4096
